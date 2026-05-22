2a1fb6cbb23e700a919547840d031042d4760a08
alishazimov <alish.azimov@icloud.com>
2026-05-22 11:39:32 +0500

    developing delta procedure

--- src/sql/01_raw/002_create_raw_load_procedures.sql
+++ src/sql/01_raw/002_create_raw_load_procedures.sql
@@ -1,6 +1,6 @@
-
-
+--------------------------------------------------------------------------
 -- Создание функции и процедур для логирования процесса загрузки данных --
+--------------------------------------------------------------------------
 
 create or replace function meta.start_batch(p_process_name text)
 returns bigint
@@ -53,9 +53,9 @@ begin
 end;
 $$;
 
------------------------------------------------------------------------------------
-
+--------------------------------------------------------------------------
 -- Процедура полной первичной загрузки данных bookings в RAW-слой --
+--------------------------------------------------------------------------
 
 create or replace procedure raw.load_bookings_initial()
 language plpgsql
@@ -112,8 +112,9 @@ exception
 end;
 $$;
 
-
+--------------------------------------------------------------------------
 -- Первичное заполнение raw.bookings_snapshot данными из источника --
+--------------------------------------------------------------------------
 
 create or replace procedure raw.init_bookings_snapshot()
 language plpgsql
@@ -150,150 +151,108 @@ end if;
 end 
 $$;
 
+--------------------------------------------------------------------------
+-- Delta-загрузка bookings в RAW через snapshot CDC imitation --
+--------------------------------------------------------------------------
 
+-- Подготовка временного снимка данных source_fdw.bookings для расчёта delta
 
-
-create or replace procedure raw.load_bookings_delta()
+create or replace procedure raw.prepare_bookings_delta_source()
 language plpgsql
 as $$
-declare
-    v_batch_id bigint;
 begin
-    -- Создаем запись о запуске загрузки и получаем batch_id
-    v_batch_id := meta.start_batch('raw.load_bookings_delta');
+	
+	drop table if exists tmp_source_bookings;
+	
+	create temp table tmp_source_bookings
+	-- on commit drop
+	as
+		select
+			b.book_ref,
+			b.book_date,
+			b.total_amount,
+			md5(
+			    concat_ws('|',
+			        coalesce(b.book_ref, ''),
+			        coalesce(b.book_date::text, ''),
+			        coalesce(b.total_amount::text, '')
+			    )
+			) as raw_row_hash
+		from source_fdw.bookings b;
+end;
+$$;
 
-    begin
-        -- Временная таблица с текущим состоянием source и рассчитанным hash
-        create temp table tmp_source_bookings
-        on commit drop
-        as
-        select
-            b.book_ref,
-            b.book_date,
-            b.total_amount,
-            md5(
-                concat_ws('|',
-                    coalesce(b.book_ref, ''),
-                    coalesce(b.book_date::text, ''),
-                    coalesce(b.total_amount::text, '')
-                )
-            ) as raw_row_hash
-        from source_fdw.bookings b;
 
-        -- I: новые строки, которых нет в snapshot
-        insert into raw.bookings (
-            book_ref,
-            book_date,
-            total_amount,
-            record_source,
-            source_system,
-            batch_id,
-            operation_type,
-            raw_row_hash
-        )
-        select
-            src.book_ref,
-            src.book_date,
-            src.total_amount,
-            'demo.bookings.bookings' as record_source,
-            'postgres_demo' as source_system,
-            v_batch_id as batch_id,
-            'I' as operation_type,
-            src.raw_row_hash
-        from tmp_source_bookings src
-        left join raw.bookings_snapshot snap
-            on snap.book_ref = src.book_ref
-        where snap.book_ref is null;
+-- Поиск новых строк: I
 
-        -- U: строки есть и в source, и в snapshot, но hash изменился
-        insert into raw.bookings (
-            book_ref,
-            book_date,
-            total_amount,
-            record_source,
-            source_system,
-            batch_id,
-            operation_type,
-            raw_row_hash
-        )
-        select
-            src.book_ref,
-            src.book_date,
-            src.total_amount,
-            'demo.bookings.bookings' as record_source,
-            'postgres_demo' as source_system,
-            v_batch_id as batch_id,
-            'U' as operation_type,
-            src.raw_row_hash
-        from tmp_source_bookings src
-        join raw.bookings_snapshot snap
-            on snap.book_ref = src.book_ref
-        where src.raw_row_hash <> snap.raw_row_hash;
+create or replace procedure raw.insert_bookings_delta_i(p_batch_id bigint)
+language plpgsql
+as $$
+begin 
+	insert
+	into
+	raw.bookings(
+		book_ref,
+		book_date,
+		total_amount,
+		record_source,
+		source_system,
+		batch_id,
+		operation_type,
+		raw_row_hash
+	)
+	select 
+		src.book_ref,
+		src.book_date,
+		src.total_amount,
+		'demo.bookings.bookings',
+		'postgres_demo',
+		p_batch_id,
+		'I',
+		src.raw_row_hash
+	
+	from
+		tmp_source_bookings src
+    left join raw.bookings_snapshot sp on src.book_ref = sp.book_ref
+    where sp.book_ref is null;
+	
+end;
+$$;
 
-        -- D: строки были в snapshot, но исчезли из source
-        insert into raw.bookings (
-            book_ref,
-            book_date,
-            total_amount,
-            record_source,
-            source_system,
-            batch_id,
-            operation_type,
-            raw_row_hash
-        )
-        select
-            snap.book_ref,
-            snap.book_date,
-            snap.total_amount,
-            'demo.bookings.bookings' as record_source,
-            'postgres_demo' as source_system,
-            v_batch_id as batch_id,
-            'D' as operation_type,
-            snap.raw_row_hash
-        from raw.bookings_snapshot snap
-        left join tmp_source_bookings src
-            on src.book_ref = snap.book_ref
-        where src.book_ref is null;
 
-        -- Обновляем snapshot: новые и изменённые строки
-        insert into raw.bookings_snapshot (
-            book_ref,
-            book_date,
-            total_amount,
-            raw_row_hash,
-            last_seen_at
-        )
-        select
-            src.book_ref,
-            src.book_date,
-            src.total_amount,
-            src.raw_row_hash,
-            now() as last_seen_at
-        from tmp_source_bookings src
-        on conflict (book_ref) do update
-        set
-            book_date = excluded.book_date,
-            total_amount = excluded.total_amount,
-            raw_row_hash = excluded.raw_row_hash,
-            last_seen_at = now();
 
-        -- Удаляем из snapshot строки, которых больше нет в source
-        delete from raw.bookings_snapshot snap
-        where not exists (
-            select 1
-            from tmp_source_bookings src
-            where src.book_ref = snap.book_ref
-        );
+-- Изменённые строки: U
 
-        -- Успешное завершение batch
-        call meta.finish_batch(v_batch_id);
+create or replace procedure raw.insert_bookings_delta_u(p_batch_id bigint)
+language plpgsql
+as $$
+begin 
+	insert
+	into
+	raw.bookings(
+		book_ref,
+		book_date,
+		total_amount,
+		record_source,
+		source_system,
+		batch_id,
+		operation_type,
+		raw_row_hash
+	)
+	select 
+		src.book_ref,
+		src.book_date,
+		src.total_amount,
+		'demo.bookings.bookings',
+		'postgres_demo',
+		p_batch_id,
+		'U',
+		src.raw_row_hash
+	from
+		tmp_source_bookings src
+    join raw.bookings_snapshot sp on src.book_ref = sp.book_ref
+    where src.raw_row_hash!=sp.raw_row_hash;
 
-    exception
-        when others then
-            call meta.fail_batch(v_batch_id, sqlerrm);
-
-            raise notice 'raw.load_bookings_delta failed: %', sqlerrm;
-    end;
 end;
 $$;
 
@@ -302,8 +261,55 @@ $$;
 
 
 
-call raw.load_bookings_initial();
-call raw.init_bookings_snapshot();
-call raw.load_bookings_delta();
+
+update source_fdw.bookings
+set total_amount = 2000.00
+where book_ref = 'TST001';
 
 
+
+
+
+
+insert into source_fdw.bookings (
+    book_ref,
+    book_date,
+    total_amount
+)
+values (
+    'TST001',
+    now(),
+    1000.00
+);
+
+
+
+
+call raw.prepare_bookings_delta_source();
+select meta.start_batch('test_insert_bookings_delta_i');
+call raw.insert_bookings_delta_i(3);
+
+select
+    book_ref,
+    book_date,
+    total_amount,
+    batch_id,
+    operation_type,
+    raw_row_hash
+from raw.bookings
+where batch_id = 3;
+
+select operation_type, count(*)
+from raw.bookings
+where batch_id = 999
+group by operation_type;
+
+------------------------------------------------------
+
+
+--call raw.load_bookings_initial();
+--call raw.init_bookings_snapshot();
+--call raw.load_bookings_delta();
+--
+--
+--select * from meta.load_batch
